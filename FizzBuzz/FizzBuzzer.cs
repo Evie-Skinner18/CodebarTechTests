23776d83b952323e5b701374c444e4033d0676e6
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-05-05 13:32:53 +0100

    Relearning the difference between having many ifs and having one if plus some else ifs

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -6,6 +6,7 @@ namespace CodebarTests
     {
         private readonly int _numLimit;
         private string _message = "out of the for loop";
+        //private readonly int _remainder = 0;
 
 
         public FizzBuzzer(int numLimit)
@@ -14,26 +15,38 @@ namespace CodebarTests
         }
 
 
-        public string FizzBuzz()
+        public void FizzBuzz()
         {
-            for (int i = 0; i < _numLimit; i++)
+            for(int i = 0; i < _numLimit; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("Fizz Buzz");
                 }
 
-                if (i % 3 == 0)
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
 
-                if (i % 5 == 0)
+                else if (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
 
                 Console.WriteLine(i);
+
+                //switch (true)
+                //{
+                //    case (i % 3 == 0 && i % 5 == 0):
+                //        Console.WriteLine("Fizz Buzz");
+                //        break;
+
+                //    default:
+                //        break;
+                //}
+
+
             }
             Console.WriteLine(_message);
         }
