673b9ceb0148343573e21065e7da79a907df0304
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-05-05 13:16:16 +0100

    Is there a way to do this without loads of ifs?

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -1,10 +1,42 @@
 ﻿using System;
-namespace FizzBuzz
+namespace CodebarTests
 {
+
     public class FizzBuzzer
     {
-        public FizzBuzzer()
+        private readonly int _numLimit;
+        private string _message = "out of the for loop";
+
+
+        public FizzBuzzer(int numLimit)
         {
+            _numLimit = numLimit;
         }
+
+
+        public string FizzBuzz()
+        {
+            for (int i = 0; i < _numLimit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    return "Fizz Buzz";
+                }
+
+                if (i % 3 == 0)
+                {
+                    return "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    return "Buzz";
+                }
+
+                return i.ToString();
+            }
+            return _message;
+        }
+
     }
 }
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,11 @@ namespace CodebarTests
     {
         static void Main(string[] args)
         {
+            var fizzBuzzer = new FizzBuzzer(10);
+
             Console.WriteLine("Welcome to the Fizz Buzzer!");
+            Console.WriteLine(fizzBuzzer.FizzBuzz());
+            Console.ReadKey();
         }
     }
 }
