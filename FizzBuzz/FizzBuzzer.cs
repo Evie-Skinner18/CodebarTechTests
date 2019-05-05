ca80335953aa6c99a11b9efe9c4e13514a547e6c
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-05-05 13:35:17 +0100

    Tidied up the code

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -5,9 +5,6 @@ namespace CodebarTests
     public class FizzBuzzer
     {
         private readonly int _numLimit;
-        private string _message = "out of the for loop";
-        //private readonly int _remainder = 0;
-
 
         public FizzBuzzer(int numLimit)
         {
@@ -19,36 +16,15 @@ namespace CodebarTests
         {
             for(int i = 0; i < _numLimit; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("Fizz Buzz");
-                }
+                if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("Fizz Buzz");
 
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
+                else if (i % 3 == 0) Console.WriteLine("Fizz");
 
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
+                else if (i % 5 == 0) Console.WriteLine("Buzz");
 
-                Console.WriteLine(i);
-
-                //switch (true)
-                //{
-                //    case (i % 3 == 0 && i % 5 == 0):
-                //        Console.WriteLine("Fizz Buzz");
-                //        break;
-
-                //    default:
-                //        break;
-                //}
-
-
+                else Console.WriteLine(i);
             }
-            Console.WriteLine(_message);
+
         }
 
     }
