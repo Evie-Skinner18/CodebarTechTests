a5916eafb3df3b97c76065e6a6da048a0ab4b36c
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-05-05 15:42:03 +0100

    Made a Bank Account class with two methods

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        private readonly string _option;
+        public int Balance;
+        public List<string> Transactions;
+
+
+        public BankAccount(string option) => _option = option;
+
+
+        public string DepositFunds(int funds)
+        {
+            return (Balance += funds).ToString();
+        }
+
+        public string WithdrawFunds(int withdrawalAmount)
+        {
+            return withdrawalAmount > Balance ? "Withdrawal blocked! You don't have enough money soz."
+        }
+
+    }
+}
--- Bank/Program.cs
+++ Bank/Program.cs
@@ -6,7 +6,14 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Gringotts Bank! Type the number of one of the following options to continue...");
+            Console.WriteLine("1: Deposit \n 2: Withrawal \n 3: Print account statement");
         }
     }
 }
+
+//User can interact with the code using a REPL like IRB or with the JavaScript console.
+//User can make deposits and withdrawals.
+//User should not be able to withdraw more money than they have in their account.
+//User can print their account statement which should show date, amount withdrawn/deposited and balance.
+//The account statement should be printed in descending order (latest date first).
