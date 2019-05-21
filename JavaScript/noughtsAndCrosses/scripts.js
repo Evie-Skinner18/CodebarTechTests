6799eeca020f20498d87b459211f2b79f3a850ed
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-21 17:19:57 +0100

    Started the noughts and crosses challenge. My boxes are not showing up yet

--- JavaScript/noughtsAndCrosses/scripts.js
+++ JavaScript/noughtsAndCrosses/scripts.js
@@ -1,5 +1,11 @@
 
 
-//alert('Connected!');
+// There are two players - X and O.
+// The game is played on a 3 x 3 grid.
+// Players alternate to place Xs and Os on the grid until either wins.
+// A player can only place their piece in an empty square.
+// A player's turn is over once they have placed their piece.
+// The first player to get 3 of their pieces in a row (up, down, across or diagonally) wins.
+// When all 9 squares are full, the game is over. If no player has 3 pieces in a row, the game ends in a tie.
 
-document.write('Connected!');
+//document.write('Connected!');
