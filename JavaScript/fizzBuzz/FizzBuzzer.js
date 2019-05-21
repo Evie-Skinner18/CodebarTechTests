11aecbf3a274db702f03ec0a438edea451ff5504
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-21 13:46:01 +0100

    Made a FizzBuzzer class, applying what I learnt from the C# version. Started writing the method

---
+++ JavaScript/fizzBuzz/FizzBuzzer.js
@@ -0,0 +1,16 @@
+
+// when a FizzBuzzer object gets instantiated, it will have a limit of nums to operate on from the start
+class FizzBuzzer {
+    constructor(numLimit) {
+        this.numLimit = numLimit;
+    };
+
+
+    // method
+     fizzBuzz(numLimit) {
+        
+        for(i = 0; i < numLimit.length; i++) {
+            
+        }
+    }
+}
