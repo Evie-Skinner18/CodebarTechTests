829967ddd0c810d184e64a2f9c306a6e0168546b
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-21 16:55:59 +0100

    Trying to take user input using node but node documentation is a bit dense

--- JavaScript/fizzBuzz/fizzBuzz.js
+++ JavaScript/fizzBuzz/fizzBuzz.js
@@ -8,16 +8,29 @@ console.log('fizzBuzz is running');
 // in node you have to istantiate a readline object in order to take user input
 let lineReader = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    prompt: 'Welcome to the Fizz Buzzer! Enter a number and I will tell you if it is a Fizz, a Buzz,' +
+    ' or a Fizz Buzz.'
 });
 
-lineReader.question(('Welcome to the Fizz Buzzer! Enter a number and I will tell you if it is a Fizz, a Buzz,' +
-' or a Fizz Buzz.', (answer) => {
-    let userNumber = parseInt(answer);
-    let fizzBuzzer = new FizzBuzzer(userNumber);
-    fizzBuzzer.fizzBuzz();
+// lineReader.question(('Welcome to the Fizz Buzzer! Enter a number and I will tell you if it is a Fizz, a Buzz,' +
+// ' or a Fizz Buzz.', (answer) => {
+//     let userNumber = parseInt(answer);
+//     let fizzBuzzer = new FizzBuzzer(userNumber);
+//     fizzBuzzer.fizzBuzz();
 
-    lineReader.close();
-}))
+//     lineReader.close();
+// }))
+
+lineReader.prompt();
+lineReader.on('line', (line) => {
+    let userNumber = parseInt(line);
+    let fizzBuzzer = new FizzBuzzer(userNumber);
+    fizzBuzzer.fizzBuzz();    
+},
+    lineReader.close());
+
+
+//console.log(userInput);
 
 
