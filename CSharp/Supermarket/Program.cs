17416fef38e3c40de579aca40a8f56cd6b6c8f5e
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-28 15:15:41 +0100

    Added supermarket project and unit test project

---
+++ CSharp/Supermarket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to the Super DUPER Market!");
+        }
+    }
+}
