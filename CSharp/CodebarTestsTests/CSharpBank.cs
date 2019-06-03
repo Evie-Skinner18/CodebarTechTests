9cae1863ebca5b79d4c12a5e7a9cbf87d391c5e7
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-06-03 14:04:48 +0100

    Added tests for C# fizzbuzz

---
+++ CSharp/CodebarTestsTests/CSharpBank.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodebarTestsTests
+{
+    [TestClass]
+    public class CSharpBank
+    {
+
+    }
+}
--- CSharp/CodebarTestsTests/CSharpFizzBuzz.cs
+++ CSharp/CodebarTestsTests/CSharpFizzBuzz.cs
@@ -6,11 +6,50 @@ namespace CodebarTestsTests
     [TestClass]
     public class CSharpFizzBuzz
     {
+        
         [TestMethod]
-        public void CanReturnFizzBuzz_ShouldReturnTrue()
+        public void CanReturnFizzBuzzFor0_ShouldReturnTrue()
         {
             var fizzBuzzer = new FizzBuzzer(0);
             var message = fizzBuzzer.FizzBuzz();
+
+            Assert.AreNotEqual(message, null);
+            Assert.AreEqual(message, "Fizz Buzz");
+        }
+
+        [TestMethod]
+        public void CanReturnFizzFor3_ShouldReturnTrue()
+        {
+            var fizzBuzzer = new FizzBuzzer(3);
+            var message = fizzBuzzer.FizzBuzz();
+
+            Assert.AreNotEqual(message, null);
+            Assert.AreEqual(message, "Fizz");
+        }
+
+        [TestMethod]
+        public void CanReturnBuzzFor5_ShouldReturnTrue()
+        {
+            var fizzBuzzer = new FizzBuzzer(5);
+            var message = fizzBuzzer.FizzBuzz();
+
+            Assert.AreNotEqual(message, null);
+            Assert.AreEqual(message, "Buzz");
+        }
+
+        [TestMethod]
+        public void CanNotReturnFizzBuzzOrFizzBuzzFor7_ShouldReturnTrue()
+        {
+            var fizzBuzzer = new FizzBuzzer(7);
+            var message = fizzBuzzer.FizzBuzz();
+
+            Assert.AreNotEqual(message, null);
+
+            Assert.AreNotEqual(message, "Fizz");
+            Assert.AreNotEqual(message, "Buzz");
+            Assert.AreNotEqual(message, "Fizz Buzz");
+
+            Assert.AreEqual(message, "7");
         }
     }
 }
