80b5dbd5341b770a5eea3622c64d3893c30fc29b
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-06-03 14:59:56 +0100

    STarted the supermarket tests

---
+++ CSharp/CodebarTestsTests/CSharpSuperMarket.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Supermarket;
+
+
+namespace CodebarTestsTests
+{
+    [TestClass]
+    public class CSharpSuperMarket
+    {
+        private List<Product> _oneOfEachItemShoppingTrolley = new List<Product>
+        {
+            new Product("Fruit tea", 3.11),
+            new Product("Box of coffee", 11.23),
+            new Product("Punnet of strawberries", 5.00)
+        };
+
+        private List<Product> _strawbsTrolley = new List<Product>
+        {
+            new Product("Punnet of strawberries", 5.00),
+            new Product("Punnet of strawberries", 5.00),
+            new Product("Punnet of strawberries", 5.00),
+            new Product("Punnet of strawberries", 5.00),
+            new Product("Punnet of strawberries", 5.00)
+        };
+
+        private List<Product> _twoOfEachItemShoppingTrolley = new List<Product>
+        {
+            new Product("Fruit tea", 3.11),
+            new Product("Fruit tea", 3.11),
+            new Product("Box of coffee", 11.23),
+            new Product("Box of coffee", 11.23),
+            new Product("Punnet of strawberries", 5.00),
+            new Product("Punnet of strawberries", 5.00)
+        };
+
+        [TestMethod]
+        public void CanCountAllItemsInTrolley_ShouldReturnFive()
+        {
+
+        }
+    }
+}
